ef922366a0e5434d441c45b423a6da0fa1114399
Daniel Dornhardt <daniel@dornhardt.com>
2015-10-10 16:56:49 +0200

    Adding Schema

--- src/lib/collections.js
+++ src/lib/collections.js
@@ -1,3 +1,31 @@
-var HackABomb = {};
+
 HackABomb.Games = new Mongo.Collection('games');
 HackABomb.BombVectors = new Mongo.Collection('bombvectors');
+
+HackABomb.Games.Schema = new SimpleSchema({
+
+});
+
+var VectorSchema = new SimpleSchema({
+    x: {
+        type: Number
+    },
+    y: {
+        type: Number
+    }
+});
+
+HackABomb.BombVectors.Schema = new SimpleSchema({
+    game_id: {
+        type: String,
+        regEx: SimpleSchema.RegEx.Id
+    },
+    position: {
+        type: VectorSchema
+    },
+    motion: {
+        type: VectorSchema
+    }
+});
+
+HackABomb.BombVectors.attachSchema(HackABomb.BombVectors.Schema);
